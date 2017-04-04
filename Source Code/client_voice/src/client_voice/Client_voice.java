4f2383035d4b3a0154659fb8101d0f8e8e8e59db
phuuvinh <vinhphamce@outlook.com>
2017-04-04 20:30:19 +0700

    update

--- Source Code/client_voice/src/client_voice/Client_voice.java
+++ Source Code/client_voice/src/client_voice/Client_voice.java
@@ -9,7 +9,7 @@ public class Client_voice {
 
     public static boolean calling = false;
     public static void main(String[] args) {
-        client_fr fr = new client_fr();
+        client_form fr = new client_form();
         fr.setVisible(true);
     }
     

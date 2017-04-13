0191616f1b890f91c04097218987ad3af8350206
phuuvinh <vinhphamce@outlook.com>
2017-04-13 23:18:11 +0700

    update: client

--- Source Code/client/src/client/Client.java
+++ Source Code/client/src/client/Client.java
@@ -5,37 +5,33 @@
  */
 package client;
 
-import java.net.*;
-import java.io.*;
-import java.util.*;
-/**
- *
- * @author Vinh
- */
-public class Client extends Thread {
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
-private Socket soc; 
-private record rec;                                        
-private ArrayList<play> pl = new ArrayList<play>();         
 
-    public Client (String svIP, int svPort) throws UnknownHostException, IOException {
-            soc =new Socket(svIP,svPort);
-    }
+public class client {
+    //call's status
+    public static boolean calling = false;
+    public static boolean invite = false;
+    public static boolean acceptToCall = false;
+    public static Socket sock;
+    //another user's info
+    public static InetAddress callTo;
     
-    @Override
-    public void run(){
-        try{
-            ObjectInputStream in = new ObjectInputStream(soc.getInputStream());
-            ObjectOutputStream out =new ObjectOutputStream(soc.getOutputStream());
-            try{
-                rec =new record(rec);
-                rec.start();
-            }
-                 
-        }catch( ){
-            
-        }
+    public static InetAddress myID;
+    public static void main(String[] args) throws UnknownHostException {
+        call callFr = new call();       
         
+        callFr.setVisible(true);
+        while(true){
+            if (invite){
+                incoming comingFr = new incoming();                
+                comingFr.setVisible(true);
+            }
+        }
     }
     
 }

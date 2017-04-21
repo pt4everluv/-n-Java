4078abd8753259ee84767102e07c2440af4bb426
DoVnTi <dovnti18051996@gmail.com>
2017-04-21 10:51:27 +0700

    Client gui

---
+++ Source Code/CLIENT_GUI/APP_WIN/src/MAIN/MAIN.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MAIN;
+
+import WINDOWS.WIN_HOME;
+/**
+ *
+ * @author DOVNTI
+ */
+
+public class MAIN{
+
+  
+    public static void main(String[] args) 
+    {   
+       WIN_HOME WH = new WIN_HOME();
+       WH.setVisible(true);
+       
+       //System.out.print("dasdasd");
+    }
+}
